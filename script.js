9d1d34cf78fe14b967501eb1c1b322fb88a841b3
rhenamaeancajas-coder <rhenamaeancajas@gmail.com>
2025-08-12 15:22:07 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple form handler
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Thank you for your message! I will get back to you soon.");
+});
